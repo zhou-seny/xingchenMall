2ef50f7f9a0b0e461bc851f6e1f40dc9e0f47417
seny_zhou <zme4130@163.com>
2023-12-19 15:31:41 +0800

    fix(mapper): 通用mapper自定义接口查询sql修改(spec)

--- changgou_service/changgou_service_goods/src/main/java/com/changgou/goods/dao/SpecMapper.java
+++ changgou_service/changgou_service_goods/src/main/java/com/changgou/goods/dao/SpecMapper.java
@@ -13,6 +13,6 @@ public interface SpecMapper extends Mapper<Spec> {
     /*
     根据分类名称查询规格列表
     */
-    @Select("select name, options from tb_spec where template_id in (select template_id from tb_category where name=#{name}));")
+    @Select("select name, options from tb_spec where template_id in (select template_id from tb_category where name=#{name});")
     public List<Map> findSpecListByCategoryName(@Param("name") String name);
 }
