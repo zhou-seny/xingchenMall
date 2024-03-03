aed817a8395238e23668abd1631a5772b3dca29c
zhou_seny <zme4130@163.com>
2024-03-03 21:57:36 +0800

    feat(api): feign过滤器增强请求头信息（令牌）

--- changgou_web/changgou_web_order/src/main/java/com/changgou/web/order/WebCartAppllication.java
+++ changgou_web/changgou_web_order/src/main/java/com/changgou/web/order/WebCartAppllication.java
@@ -1,9 +1,11 @@
 package com.changgou.web.order;
 
+import com.changgou.interceptor.FeignInterceptor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @EnableEurekaClient
@@ -12,5 +14,10 @@ public class WebCartAppllication {
 
     public static void main(String[] args) {
         SpringApplication.run(WebCartAppllication.class, args);
+    }
+
+    @Bean
+    public FeignInterceptor feignInterceptor(){
+        return new FeignInterceptor();
     }
 }
