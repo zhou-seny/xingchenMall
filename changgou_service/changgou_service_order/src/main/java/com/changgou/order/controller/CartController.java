9ace9b09cd1131ebf04c525ca0941fb5a315cc9d
zhou_seny <zme4130@163.com>
2024-03-03 22:07:42 +0800

    feat(api): 动态获取登录者

--- changgou_service/changgou_service_order/src/main/java/com/changgou/order/controller/CartController.java
+++ changgou_service/changgou_service_order/src/main/java/com/changgou/order/controller/CartController.java
@@ -3,6 +3,7 @@ package com.changgou.order.controller;
 
 import com.changgou.entity.Result;
 import com.changgou.entity.StatusCode;
+import com.changgou.order.config.TokenDecode;
 import com.changgou.order.service.CartService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,17 +19,23 @@ public class CartController {
     @Autowired
     private CartService cartService;
 
+    @Autowired
+    private TokenDecode tokenDecode;
+
     @RequestMapping("/addCart")
     public Result addCart(@RequestParam("skuId") String skuId, @RequestParam("num") Integer num){
 
-        String username = "zhangsan";
+
+        // String username = "zhangsan";
+        String username = tokenDecode.getUserInfo().get("username");
         cartService.addCart(skuId, num, username);
         return new Result(true, StatusCode.OK, "添加购物车成功");
     }
 
     @RequestMapping("/cartList")
     public Map cartList(){
-        String username = "zhangsan";
+        String username = tokenDecode.getUserInfo().get("username");
+        // String username = "zhangsan";
         return cartService.cartList(username);
     }
 }
